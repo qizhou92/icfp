f25434c2b0d4baca18ba7be941a515ff244dfb17
Bill Harris <wrharris@cs.wisc.edu>
2017-02-02 14:00:40 -0500

    partial entries in relational invariants of climbStairs

--- draft/code/ClimbStairs0.ml
+++ draft/code/ClimbStairs0.ml
@@ -1,10 +1,16 @@
-let climbStairs0 n =
-  (if n <= 1 then 1 (* 4 *)
-  else
-    let rec climbStairs0Rec
-        sum prev cur i = 
-      if i < n then
-        climbStairsRec
-          (sum + prev) cur sum (i + 1)
-      else sum) (* 2 *) in
-    climbStairsRec 2 1 0 2 (* 1 *)
+(fun n ->
+   (if (n <= 1 (* 2 *) ) then
+       (1 (* 3 *) )
+    else
+       ( (fix (fun cS0Rec
+         sum prev cur i ->
+           (if i < n then
+               cS0Rec
+                 (sum + prev)
+                 cur
+                 sum
+                 (i + 1)
+            else sum ) ) (* 5 *) )
+           2 1 0 2 (* 4 *) )
+   (* 1 *) )
+(* 0 *) )
--- draft/code/ClimbStairs1.ml
+++ draft/code/ClimbStairs1.ml
@@ -1,9 +1,11 @@
-let climbStairs1 n =
-  if n <= 1 then 1
-  else
-    let rec climbStairs1Rec count1 count2 i =
-      if i <= n then
-        climbStairs1Rec
-          count2 (count2 + count1) (i + 1)
-      else count2 in
-    climbStairs1Rec 1 1 2
+(fun n ->
+  (if (n <= 1 (* 2 *) ) then
+      (1 (* 3 *) )
+   else
+      ( (fix (fun cS1rec count1 count2 i ->
+        if i <= n then
+          cS1rec count2 (count2 + count1) (i + 1)
+        else count2) (* 5 *) ) 
+          1 1 2 (* 4 *) )
+  ) (* 1 *)
+(* 0 *) )
