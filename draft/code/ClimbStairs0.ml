eb031fd42839f28165e666920098014aeecde136
Bill Harris <wrharris@cs.wisc.edu>
2017-02-02 19:06:58 -0500

    draft, eval: first cut at relational invariants for ClimbingStairs

--- draft/code/ClimbStairs0.ml
+++ draft/code/ClimbStairs0.ml
@@ -1,16 +1,17 @@
 (fun n ->
-   (if (n <= 1 (* 2 *) ) then
-       (1 (* 3 *) )
+   (if (n <= 1 (* 2 *) ) then (1 (* 3 *))
     else
-       ( (fix (fun cS0Rec
-         sum prev cur i ->
-           (if i < n then
-               cS0Rec
-                 (sum + prev)
-                 cur
-                 sum
-                 (i + 1)
-            else sum ) ) (* 5 *) )
-           2 1 0 2 (* 4 *) )
+       ((fix (fun cS0Rec
+         (sum, prev, cur, i) ->
+           (if i < n (* 8 *) then
+               (cS0Rec
+                 (sum + prev,
+                  cur, sum, i + 1 (* 11 *))
+               (* 9 *))
+            else (sum (* 10 *))
+           (* 7 *)))
+        (* 5 *))
+           (2, 1, 0, 2 (* 6 *) )
+       (* 4 *) )
    (* 1 *) )
 (* 0 *) )
--- draft/code/ClimbStairs1.ml
+++ draft/code/ClimbStairs1.ml
@@ -1,11 +1,14 @@
 (fun n ->
-  (if (n <= 1 (* 2 *) ) then
-      (1 (* 3 *) )
+  (if (n <= 1 (* 2 *) ) then (1 (* 3 *))
    else
-      ( (fix (fun cS1rec count1 count2 i ->
-        if i <= n then
-          cS1rec count2 (count2 + count1) (i + 1)
-        else count2) (* 5 *) ) 
-          1 1 2 (* 4 *) )
-  ) (* 1 *)
+      ((fix (fun cS1rec (count1, count2, i) ->
+        (if i <= n (* 8 *) then
+            (cS1rec (count2, count2 + count1, i + 1 (* 11 *))
+            (* 9 *))
+         else (count2 (* 10 *))
+        (* 7 *)))
+       (* 5 *))
+          (1, 1, 2 (* 6 *) )
+      (* 4 *) )
+  (* 1 *) )
 (* 0 *) )
