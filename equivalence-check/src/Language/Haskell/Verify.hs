741d37e356ace95b77ad541035575da8c45a8df4
nikivazou <nikivazou@gmail.com>
2017-05-24 08:34:20 -0400

    merging

--- equivalence-check/src/Language/Haskell/Verify.hs
+++ equivalence-check/src/Language/Haskell/Verify.hs
@@ -8,8 +8,14 @@ module Language.Haskell.Verify (
 import Language.Haskell.Types
 import Data.Monoid 
 
-import qualified Language.Haskell.Expr as Logic 
-import qualified Data.HashMap.Strict   as M 
+-- NV This does not compile
+-- import qualified Language.Haskell.Expr as Logic 
+
+import qualified Data.Map   as M 
+
+-------------------------------------------------------------------------------
+-- | verify -------------------------------------------------------------------
+-------------------------------------------------------------------------------
 
 verify :: ProgramBind -> ProgramBind -> IO Result 
 verify (x1,p0) (x2,p1) = Result (x1, x2) <$> vAux mempty 
@@ -24,18 +30,40 @@ verify (x1,p0) (x2,p1) = Result (x1, x2) <$> vAux mempty
                               Nothing -> return False
                               Just i' -> vAux (i <> i')
 
+-------------------------------------------------------------------------------
+-- | checkInd -----------------------------------------------------------------
+-------------------------------------------------------------------------------
+
 checkInd :: Program -> Program -> DersInvs -> IO IndRes 
 checkInd _ _ _ = error "TODO: checkInd"
+
+-------------------------------------------------------------------------------
+-- | verifyDers ---------------------------------------------------------------
+-------------------------------------------------------------------------------
 
 verifyDers :: Ders -> Ders -> IO (Maybe DersInvs)
 verifyDers _ _ = error "TODO: verifyDers"
 
+-------------------------------------------------------------------------------
+-- | Data Structures ----------------------------------------------------------
+-------------------------------------------------------------------------------
+
 data IndRes   = IsInd | IndDers {_indRes0 :: Ders, _indRes1 :: Ders}
 data Ders      
-data DerCtxs
-type Invariant = Logic.Expr
-data DersInvs = DersInvs (M.HashMap (DerCtxs, DerCtxs) Invariant)
+data DerCtxs = DerCtxs
+  deriving (Eq, Ord)
+
+
+type Invariant = Expr -- = Logic.Expr
+data DersInvs = DersInvs (M.Map (DerCtxs, DerCtxs) Invariant)
 
 instance Monoid DersInvs where
-  mempty  = DersInvs
-  mappend = \_ _ -> DersInvs
+  mempty  = mempty
+  mappend (DersInvs m1) (DersInvs m2) = DersInvs $ M.unionWith mappend m1 m2 
+
+
+-- TODO replace this with Logic.Expr
+data Expr = ETrue | EAnd Expr Expr 
+instance Monoid Expr where 
+  mempty  = ETrue
+  mappend = EAnd
