1c4c17ae3fb7ee15de2e758c649d6248b12b0168
Qi zhou <qizhou@lawn-128-61-52-95.lawn.gatech.edu>
2017-06-29 15:30:06 -0400

    an example with high order function works

--- equivalence-check/src/Language/Equivalence/test5.hs
+++ equivalence-check/src/Language/Equivalence/test5.hs
@@ -15,6 +15,7 @@ main = do
   let a = (Types.Var "arg1")
   -- /y y+1
   let inside = Types.ELam y (Types.EBin Types.Plus (Types.EVar y) (Types.EInt 1))
+  let inside2 = Types.ELam y (Types.EBin Types.Plus (Types.EVar y) (Types.EInt 2))
   -- apply T x
   let app1 = Types.EApp (Types.EVar t) (Types.EVar x)
   -- /x (apply T x) 
@@ -23,8 +24,11 @@ main = do
   let lambada2 = Types.ELam t lambada1
   -- apply (/T /x (apply T x)) (/y y+1)
   let app2 = Types.EApp lambada2 inside
-  let app3 = Types.EApp app2 (Types.EVar a)
+  let app2_1 = Types.EApp lambada2 inside2
+  let app3 = Types.EApp app2 (Types.EInt 2)
+  let app3_1 = Types.EApp app2_1 (Types.EInt 1)
   let dEnv =[(a,(Types.ENil))]
   let d1 = (makeDerivations dEnv app3) !! 0
+  let d2 = (makeDerivations dEnv app3_1) !! 0
   let (node1,number) = translateDT 0 d1
-  print node1
+  verifyPairs d1 d2
