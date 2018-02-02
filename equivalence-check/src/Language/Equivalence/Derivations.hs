10eff440f4eadb1f6dd08384cf9518320f73949a
Qi zhou <qizhou1992@gmail.com>
2018-02-02 17:08:08 -0500

    add new derviation

--- equivalence-check/src/Language/Equivalence/Derivations.hs
+++ equivalence-check/src/Language/Equivalence/Derivations.hs
@@ -41,6 +41,8 @@ data UnwindResult = UnwindResult (Set.Set CoreExpr) Int
 
 
 type UnwindState a = (State UnwindResult) a
+
+type FixState a = (State (Set.Set CoreExpr)) a
 -- Unwind picks a symbolic choice and evaluates it
 
 getNewId :: UnwindState Int
@@ -56,6 +58,47 @@ unwindDer (Der RASym expr1 expr2 _ _) = do
 unwindDer (Der rule expr1 expr2 list idNumber) = do
   list1 <- mapM unwindDer list
   return (Der rule expr1 expr2 list1 idNumber)
+
+getNewDerivation :: CoreExpr ->FixState CoreExpr
+getNewDerivation e@(EInt _) =return e
+getNewDerivation e@(EBool _) =return e
+getNewDerivation e@(EVar _) =return e
+getNewDerivation e@(ENil) =return  e
+getNewDerivation (EBin op e1 e2) = do
+ newE1 <- (getNewDerivation e1)
+ newE2 <- (getNewDerivation e2)
+ return  (EBin op newE1 newE2)
+getNewDerivation (EIf e1 e2 e3) = do
+  newE1 <- (getNewDerivation e1)
+  newE2 <- (getNewDerivation e2)
+  newE3 <- (getNewDerivation e3)
+  return  (EIf newE1 newE2 newE3)
+getNewDerivation (EMatch e1 e2 v1 v2 e3) =  do
+  newE1 <- (getNewDerivation e1)
+  newE2 <- (getNewDerivation e2)
+  newE3 <- (getNewDerivation e3)
+  return  (EMatch newE1 newE2 v1 v2 newE3)
+getNewDerivation (ECon e1 e2) = do
+  newE1 <- (getNewDerivation e1)
+  newE2 <- (getNewDerivation e2)
+  return (ECon newE1 newE2)
+getNewDerivation (ELet v1 e1 e2) = do
+  newE1 <- (getNewDerivation e1)
+  newE2 <- (getNewDerivation e2)
+  return (ELet v1 newE1 newE2)
+getNewDerivation (EApp e1 e2) = do
+  newE1 <- (getNewDerivation e1)
+  newE2 <- (getNewDerivation e2)
+  return (EApp newE1 newE2)
+getNewDerivation (ELam v1 e1) = do
+  newE1 <- (getNewDerivation e1)
+  return (ELam v1 newE1 )
+getNewDerivation fixExpr@(EFix v1 e1) = do
+  set <- get
+  if (Set.member fixExpr set) then (return fixExpr)
+    else do
+           put (Set.insert fixExpr set )
+           getNewDerivation (substituteCoreExpr (v1,e1) e1)
 
 makeDerivations :: CoreExpr -> CoreExpr->UnwindState Der
 makeDerivations e@(EInt _) e2 = do
@@ -102,7 +145,7 @@ makeDerivations e@(EApp e1 e2) e3@(EApp e4 e5)= do
   idNumber <- getNewId
   d1 <- makeDerivations e1 e4
   d2 <- makeDerivations e2 e5
-  return (Der RNApp e e3 [d1,d2] idNumber) 
+  return (Der RNApp e e3 [d1,d2] idNumber)
 makeDerivations e@(ELet _ _ _) _
   = error ("makeDerivations: undefined on " ++ show e)
 makeDerivations e@ENil _
@@ -119,6 +162,8 @@ substituteCoreExpr (variable,value) originalCoreExpr = case originalCoreExpr of
   EApp c1 c2 -> EApp (substituteCoreExpr (variable,value) c1) (substituteCoreExpr (variable,value) c2)
   ELam v1 c1 -> if v1 == variable then ELam v1 c1 else ELam v1 (substituteCoreExpr (variable,value) c1)
   ELet v1 c1 c2 -> ELet v1 (substituteCoreExpr (variable,value) c1) (substituteCoreExpr (variable,value) c2)
+  EMatch e1 e2 v1 v2 e3 -> EMatch (substituteCoreExpr (variable,value) e1) (substituteCoreExpr (variable,value) e2) v1 v2 (substituteCoreExpr (variable,value) e3)
+  ECon e1 e2 -> ECon (substituteCoreExpr (variable,value) e1) (substituteCoreExpr (variable,value) e2)
   _ ->originalCoreExpr
 
 eBin :: Binop -> CoreExpr -> CoreExpr -> CoreExpr
