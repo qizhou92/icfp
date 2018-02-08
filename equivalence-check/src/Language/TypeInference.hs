2e220012c7761705e6280ead7792b9e2f09987a1
David Heath <heath.davidanthony@gmail.com>
2018-02-07 19:59:00 -0500

    rewrote substitute for types

--- equivalence-check/src/Language/TypeInference.hs
+++ equivalence-check/src/Language/TypeInference.hs
@@ -1,7 +1,9 @@
 module Language.TypeInference where
 
+import           Control.Lens
 import           Control.Monad.Except
 import           Control.Monad.State
+import           Control.Monad.Reader
 
 import           Data.Map (Map)
 import qualified Data.Map as M
@@ -50,25 +52,22 @@ class Substitutable a where
   freeTvars :: a -> Set Type
 
 instance Substitutable Type where
-  apply _ TInt              = TInt
-  apply _ TBool             = TBool
-  apply _ TNil              = TNil
-  apply su (TList t)        = TList $ apply su t
-  apply su t@(TVar _)       = M.findWithDefault t t (getSubst su)
-  apply su (TArr t1 t2)     = TArr (apply su t1) (apply su t2)
-  apply su (TPlus t1 t2)    = TPlus (apply su t1) (apply su t2)
-  apply su (TProduct t1 t2) = TProduct (apply su t1) (apply su t2)
-  apply su (TFix t1 t2)     = TFix t1 (apply (Subst (M.delete (TVar t1) (getSubst su))) t2)
+  -- | Apply a substitution table to the type by transitively applying to all
+  -- type subexpressions. Type variables are looked up in the table while fix
+  -- expressions remove their variable from the table (due to binding).
+  apply (Subst s) x = runReader (rewriteM sub x) s
+    where sub = \case
+            t@TVar{} -> M.lookup t <$> ask
+            TFix t1 t2 -> fmap (TFix t1) <$> local (M.delete (TVar t1)) (sub t2)
+            _ -> pure Nothing
 
-  freeTvars TInt             = S.empty
-  freeTvars TNil             = S.empty
-  freeTvars TBool            = S.empty
-  freeTvars (TList t)        = freeTvars t 
-  freeTvars (TVar a)         = S.singleton (TVar a)
-  freeTvars (TArr t1 t2)     = S.union (freeTvars t1) (freeTvars t2)
-  freeTvars (TProduct t1 t2) = S.union (freeTvars t1) (freeTvars t2)
-  freeTvars (TPlus t1 t2)    = S.union (freeTvars t1) (freeTvars t2)
-  freeTvars (TFix x t)       = S.delete (TVar x) (freeTvars t) 
+  -- | To compute the free variables, combine the sets from all subexpressions.
+  -- Variables add a free variable whereas fix expressions bind one.
+  freeTvars = para (\t ts ->
+    let ts' = mconcat ts in case t of
+      TVar a -> S.insert (TVar a) ts'
+      TFix x _ -> S.delete (TVar x) ts'
+      _ -> ts')
 
 instance Substitutable Scheme where
   apply subSet (Forall boundVars t) = Forall boundVars (apply newSubSet t)
@@ -85,24 +84,23 @@ instance Substitutable TypeEnv where
 
 type HM a = ExceptT String (State Int) a
 
-mgu :: Type -> Type -> HM Subst
-
-mgu (TArr t1 t2) (TArr t3 t4) = do s1 <- mgu t1 t3
-                                   s2 <- mgu (apply s1 t2) (apply s1 t4)
-                                   return (s2 <> s1)
-
-mgu (TVar a) t = varAsgn (TVar a) t
-mgu t (TVar a) = varAsgn (TVar a) t 
-mgu TInt TInt = return mempty
-mgu TBool TBool = return mempty
-mgu (TList a) (TList b) = mgu a b 
-mgu t1 t2 = throwError ("types do not unify: " ++ show t1 ++ " vs ." ++ show t2 ++"\n")
+unify :: Type -> Type -> HM Subst
+unify (TArr t1 t2) (TArr t3 t4) = do
+  s1 <- unify t1 t3
+  s2 <- unify (apply s1 t2) (apply s1 t4)
+  pure (s2 <> s1)
+unify (TVar a) t = varAsgn (TVar a) t
+unify t (TVar a) = varAsgn (TVar a) t 
+unify TInt TInt = pure mempty
+unify TBool TBool = pure mempty
+unify (TList a) (TList b) = unify a b 
+unify t1 t2 = throwError ("types do not unify: " ++ show t1 ++ " vs ." ++ show t2 ++"\n")
 
 varAsgn :: Type -> Type -> HM Subst
 varAsgn a t
-  | t == a         = return mempty
+  | t == a         = pure mempty
   | S.member a (freeTvars t) = throwError ("occur check fails: " ++ show a ++ " in " ++ show t)
-  | otherwise      = return (Subst $ M.singleton a t)
+  | otherwise      = pure (Subst $ M.singleton a t)
 
 generalize :: TypeEnv -> Type -> Scheme
 
@@ -114,50 +112,50 @@ instantiate :: Scheme -> HM Type
 instantiate (Forall as t) = do
   as' <- mapM (const freshTVar) as
   let s = M.fromList (zip as as')
-  return (apply (Subst s) t)
+  pure (apply (Subst s) t)
 
 freshTVar :: HM Type
 freshTVar = do
   s <- get
   put (s+1)
-  return (TVar ("newTypeVariable" ++ show s))
+  pure (TVar ("newTypeVariable" ++ show s))
 
 data TypeResult = TypeResult Subst Type (Map CoreExpr Type)
   deriving(Show)
 
 ti :: TypeEnv -> CoreExpr -> HM TypeResult
-ti _ expr@(EInt  _) = return (TypeResult mempty TInt (M.singleton expr TInt))
-ti _ expr@(EBool _) = return (TypeResult mempty TBool (M.singleton expr TBool))
+ti _ expr@(EInt  _) = pure (TypeResult mempty TInt (M.singleton expr TInt))
+ti _ expr@(EBool _) = pure (TypeResult mempty TBool (M.singleton expr TBool))
 ti _ ENil = do
   a <- freshTVar
-  return $ TypeResult mempty (TList a) mempty
+  pure $ TypeResult mempty (TList a) mempty
 ti (TypeEnv env) expr@(EVar x) = 
   case M.lookup (EVar x) env of
      Nothing -> throwError ("Unbound Variable: " ++ show x)
      Just s  -> do
        t <- instantiate s
-       return (TypeResult mempty t (M.singleton expr t))
+       pure (TypeResult mempty t (M.singleton expr t))
 
 ti tEnv expr@(EBin op expr1 expr2) = do
   TypeResult s1 t1 m1 <- ti tEnv expr1
   TypeResult s2 t2 m2 <- ti tEnv expr2
   tv <- freshTVar
   opType <- instantiate $ getOp op
-  s3 <- mgu (TArr t1 (TArr t2 tv)) opType
+  s3 <- unify (TArr t1 (TArr t2 tv)) opType
   let tnew = apply s3 tv
   let m3 = M.insert expr tnew (m1 <> m2)
-  return (TypeResult (mconcat [s1, s2, s3]) tnew m3)
+  pure (TypeResult (mconcat [s1, s2, s3]) tnew m3)
 
 ti tEnv expr@(EIf expr1 expr2 expr3) = do
   TypeResult s1 t1 m1 <- ti tEnv expr1
   TypeResult s2 t2 m2 <- ti tEnv expr2
   TypeResult s3 t3 m3 <- ti tEnv expr3
-  s4 <- mgu t1 TBool
-  s5 <- mgu t2 t3
+  s4 <- unify t1 TBool
+  s5 <- unify t2 t3
   let tnew = apply s5 t2
   let m5 = M.insert expr tnew (m1 <> m2 <> m3)
   let newSubSet = mconcat [s5, s4, s3, s2, s1]
-  return (TypeResult newSubSet tnew m5)
+  pure (TypeResult newSubSet tnew m5)
 
 ti (TypeEnv env) expr@(ELet var1 expr1 expr2) = do
   TypeResult s1 t1 m1 <- ti (TypeEnv env) expr1
@@ -166,17 +164,17 @@ ti (TypeEnv env) expr@(ELet var1 expr1 expr2) = do
   TypeResult s2 t2 m2 <- ti  (TypeEnv (M.insert (EVar var1) t' env')) expr2
   let m3 = M.union m1 m2
   let m4 = M.insert expr t2 m3
-  return (TypeResult (s1 <> s2) t2 m4) 
+  pure (TypeResult (s1 <> s2) t2 m4) 
 
 ti (TypeEnv env) expr@(EApp expr1 expr2) = do
   tv <- freshTVar
   TypeResult s1 t1 m1 <- ti (TypeEnv env) expr1
   TypeResult s2 t2 m2 <- ti (apply s1 (TypeEnv env)) expr2
-  s3 <- mgu (apply s2 t1) (TArr t2 tv)
+  s3 <- unify (apply s2 t1) (TArr t2 tv)
   let m3 = M.union m1 m2
   let tnew = apply s3 tv
   let m4 = M.insert expr tnew m3
-  return (TypeResult (mconcat [s3, s2, s1]) tnew m4)
+  pure (TypeResult (mconcat [s3, s2, s1]) tnew m4)
 
 ti (TypeEnv env) expr@(ELam var1 expr1) = do
   tv <- freshTVar
@@ -184,29 +182,30 @@ ti (TypeEnv env) expr@(ELam var1 expr1) = do
   TypeResult s1 t1 m1 <- ti (TypeEnv env') expr1
   let tnew = apply s1 (TArr tv t1)
   let m2 = M.insert expr tnew m1
-  return (TypeResult s1 tnew m2)
+  pure (TypeResult s1 tnew m2)
 
 -- not sure the rule for fix is correct
 ti (TypeEnv env) expr@(EFix var1 expr1) = do 
   tv <- freshTVar
   let env' = M.insert (EVar var1) (Forall [] tv) env
   TypeResult s1 t1 m1 <- ti (TypeEnv env') expr1
-  s2 <- mgu tv t1
+  s2 <- unify tv t1
   let tnew = apply s2 tv
   let m2 = M.insert expr tnew m1
-  return (TypeResult (s1 <> s2) tnew m2)
+  pure (TypeResult (s1 <> s2) tnew m2)
+
 ti γ@(TypeEnv env) expr@(EMatch e e1 x y e2) = do 
   TypeResult s0 te me <- ti γ e
   tv <- freshTVar 
-  se <- mgu te (TList tv)
+  se <- unify te (TList tv)
   TypeResult s1 t1 m1 <- ti γ e1
   let env' = M.insert (EVar y) (generalize γ $ TList tv) $ M.insert (EVar x) (generalize γ tv) env
   TypeResult s2 t2 m2 <- ti (TypeEnv env') e2
-  s12 <- mgu (apply se t1) (apply se t2)
+  s12 <- unify (apply se t1) (apply se t2)
   let s = mconcat [s12, s2, s1, s2, s0]
   let texpr = apply s t1
   let m = M.insert expr texpr (mconcat [me, m1, m2])
-  return (TypeResult s texpr m)
+  pure (TypeResult s texpr m)
 
 -- for Eq and Ne, it is not accurate 
 getOp :: Binop -> Scheme
@@ -226,11 +225,8 @@ getOp = \case
 binType :: Type -> Type -> Type -> Scheme
 binType t1 t2 t3 = Forall [] (TArr t1 (TArr t2 t3))
 
-getPairFresh :: HM (Type,Type)
-getPairFresh = do
-  t1 <- freshTVar
-  t2 <- freshTVar
-  return (t1,t2)
+getPairFresh :: HM (Type, Type)
+getPairFresh = (,) <$> freshTVar <*> freshTVar
 
 inferType :: CoreExpr -> Either String (Map CoreExpr Type)
 inferType expr = do
