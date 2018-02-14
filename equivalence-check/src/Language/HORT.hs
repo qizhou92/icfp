96555ede14bb63ea7014ca28cf34e8983d300508
Qi zhou <qizhou1992@gmail.com>
2018-02-14 11:58:41 -0500

    subtype check

--- equivalence-check/src/Language/HORT.hs
+++ equivalence-check/src/Language/HORT.hs
@@ -5,11 +5,18 @@ import Data.Data (Data)
 import Data.Tree
 
 import Grammar
+import Formula hiding (Rule)
 
 newtype HORT = HORT { getHORT :: Tree Nonterminal }
   deriving (Show, Read, Eq, Data)
 
 -- | Given a pair of higher order refinement types, generate the grammar rules
 -- which constrain the types.
-subtype :: HORT -> HORT -> [Rule]
-subtype = undefined
+subtype :: Category -> HORT -> HORT -> [Rule]
+subtype category hort1 hort2 = buildSubType category (getHORT hort1) (getHORT hort2)
+
+buildSubType :: Category -> (Tree Nonterminal) -> (Tree Nonterminal) -> [Rule]
+buildSubType category (Node n1 subTrees1) (Node n2 subTrees2) = do
+  let rule = Rule category n2 (LBool True) [n1]
+  let rules = concat (zipWith (buildSubType category) subTrees2 subTrees1)
+  rule:rules 
