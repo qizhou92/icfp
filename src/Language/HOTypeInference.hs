e2c1bac3112f581d94a2f20233d4e0dbf51d8d8f
David Heath <heath.davidanthony@gmail.com>
2018-02-18 11:38:00 -0500

    finished documenting HO type inference

--- src/Language/HOTypeInference.hs
+++ src/Language/HOTypeInference.hs
@@ -23,10 +23,20 @@ data InferenceError
   | InvalidSplit HORT
   deriving (Show, Read, Eq)
 
-type Ctxt = Map Var HORT
-
 type Infer a = StateT Int (ExceptT InferenceError (Writer [Rule])) a
 
+-- | Given an expression that is annotated with basic types and a context for
+-- variable basic types, generate the constraints which relate the higher
+-- order refinement types of the subexpressions.
+--
+-- Type judgements for expressions occur in two parts.
+-- First, we generate the appropriate context for each subexpression.
+-- This is done with a top down traversal of the expression. Fix expressions
+-- and lambda expressions (over non-primitive arguments) add new variables to
+-- the context.
+--
+-- Second, we perform type inference over the subexpressions, recording new
+-- constrained Horn clause rules to the Writer Monad context as we go.
 typeConstraints :: Attr CoreExpr' (Map Var Type, Type) -> Either InferenceError Grammar
 typeConstraints e =
   let e' = infer =<< contextualize <$> giveType (addFreeVars e)
@@ -40,21 +50,16 @@ typeConstraints e =
     -- | Generate a new higher order relational type for every subexpression.
     giveType = fmap unAttrib . traverse (\(m, vs, t) -> fresh m vs t) . Attrib
 
-
--- | Type judgements for expressions occur in two parts.
--- First, we generate the appropriate context for each subexpression.
--- This is done with a top down traversal of the expression. Fix expressions
--- and lambda expressions (over non-primitive arguments) add new variables to
--- the context.
---
--- Second, we perform type inference over the subexpressions, recording new
--- constrained Horn clause rules to the Writer Monad context as we go.
+type Ctxt = Map Var HORT
 
 -- | Annotate each subexpression with the context which maps variables
 -- to their corresponding context.
 contextualize :: Attr CoreExpr' HORT -> Attr CoreExpr' (HORT, Ctxt)
 contextualize = annZip . inherit (\(Fix (Ann t e)) ctxt -> case e of
+  -- We insert x into the context, regardless of its type for fix expressions.
   EFix x _ -> M.insert x t ctxt
+  -- For lambda expressions, we only add x to the context if it is not primitive.
+  -- Otherwise, we can constrain x directly.
   ELam x _ ->
     case split t of
       Just (s, _) -> if isPrim s then ctxt else M.insert x s ctxt
@@ -70,17 +75,16 @@ infer = fmap (annMap snd . annZip) .
   synthetiseM (\(Ann (t, ctxt) e) -> do
     case e of
       EVar x ->
-        if isPrim t
-        then
-          -- When x is primitive, we just match x to the output of the expression.
-          let tv = valueOf t
-              vx = F.Var (getVar x) (F.exprType tv)
-          in constrain [F.expr|$tv = @vx|] [] t
-        else do
-          -- When x is not primitive, we're forced to look up the type of x from
-          -- the context instead.
-          t' <- isearch x ctxt
-          t' <: t
+        case M.lookup x ctxt of
+          -- When x is not in the context (and hence is a primitive lambda bound variable),
+          -- we just match x to the output of the expression.
+          Nothing ->
+            let tv = valueOf t
+                vx = F.Var (getVar x) (F.exprType tv)
+            in constrain [F.expr|$tv = @vx|] [] t
+          -- When x is in the context, we say that its type in the context is a subtype
+          -- of the type of the current expression.
+          Just t' -> t' <: t
 
       EApp st s ->
         if isPrim s
@@ -100,16 +104,24 @@ infer = fmap (annMap snd . annZip) .
           s <: s'
 
       ELam x t' ->
-        if x `M.member` ctxt -- x is HO
+        if x `M.notMember` ctxt -- x is HO
         then do
-          (_ , t'') <- safeSplit t
-          t' <: t''
-        else do
+          -- When the lambda argument, x, is primitive, we can directly substitute
+          -- the bound argument for the free variable, x, in the body.
           let ta = argumentOf t
           let vx = F.Var (getVar x) (F.exprType ta)
           subtype [F.expr|$ta = @vx|] [] t' t
+        else do
+          -- When the lambda argument is not primitive, all we can do is claim that
+          -- the type of the body is a subtype of the output type of the lambda
+          -- expression.
+          (_ , t'') <- safeSplit t
+          t' <: t''
 
       EBin op r s ->
+        -- For a primitive operation, we can constrain the output of the operation
+        -- expression to be equal to performing the actual operation on the outputs
+        -- of the subexpressions.
         let rv = valueOf r
             sv = valueOf s
             tv = valueOf t
@@ -128,20 +140,31 @@ infer = fmap (annMap snd . annZip) .
         in constrain f [r, s] t
 
       EInt i ->
+        -- Int literals constrain their output to be equal to the literal.
         let tv = valueOf t
             i' = F.LInt $ toInteger i
         in constrain [F.expr|$tv = $i'|] [] t
 
       EBool b ->
+        -- Bool literals constrain their output to be equal to the literal.
         let tv = valueOf t
             b' = F.LBool b
         in constrain [F.expr|$tv = $b'|] [] t
 
       EIf s t' t'' -> do
+        -- If expressions generate two sets of constraints.
         let sv = valueOf s
+        -- One says that the consequence is a subtype of the full expression,
+        -- as witnessed by the output of the condition being true.
         subtype [F.expr|$sv|] [s] t' t
+        -- The other says taht the alternative is a subtype of the full
+        -- expression, as witnessed by the output of the condition being false.
         subtype [F.expr|not $sv|] [s] t'' t
 
+      -- Fix expressions are deceptively simple: We merely state that body of
+      -- the fix expression is a subtype of the full expression. Keep in mind
+      -- that the variable has already been bound to type `t`, causing a
+      -- recursive system of constraints.
       EFix _ t' -> t' <: t
 
       ENil -> undefined
@@ -156,14 +179,6 @@ safeSplit :: MonadError InferenceError m => HORT -> m (HORT, HORT)
 safeSplit t = case split t of
   Nothing -> throwError (InvalidSplit t)
   Just x -> pure x
-
--- | Find the type of the variable in the context or generate an error
--- if it is not in the context.
-isearch :: Var -> Ctxt -> Infer HORT
-isearch x ctxt =
-  case M.lookup x ctxt of
-    Nothing -> throwError (UnboundError x)
-    Just t' -> pure t'
 
 -- | The first type is a subtype of the second with no additional constraints.
 (<:) :: MonadWriter [Rule] m => HORT -> HORT -> m ()
