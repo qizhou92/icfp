adcc663b185c1325c447342c3764267811b2cec2
David Heath <heath.davidanthony@gmail.com>
2018-02-18 11:19:36 -0500

    cleaned up unnecessary code

--- src/Language/HORT.hs
+++ src/Language/HORT.hs
@@ -7,6 +7,7 @@ import           Data.Data (Data)
 import           Data.Tree
 import           Data.Map (Map)
 import qualified Data.Map as M
+import           Data.List (partition)
 
 import           Language.Types
 import           Grammar
@@ -37,70 +38,47 @@ argumentOf hort =
 
 -- | Whether or not this type is primitive.
 isPrim :: HORT -> Bool
-isPrim hort1 = case getBasicType hort1 of
-  TInt ->  True
-  TBool -> True
-  TArr _ _ -> False
-  _ -> error "this type should not be in HORT"
+isPrim = isPrimitiveType . getBasicType
 
 -- | Given a list of free variables and a basic type, construct
 -- a higher order refinement type.
 fresh :: MonadState Int m => Map Var Type -> [Var] -> Type -> m HORT
-fresh freeVarMaps freeVars exprType = do
-  let fvs = map (\v -> (v, freeVarMaps M.! v)) freeVars
-  let primitiveVars = filter (\(_, basicType) -> isPrimitiveType basicType) fvs
-  tree <- buildTreeNode primitiveVars exprType
-  return (HORT tree exprType)
+fresh varTypes freeVars exprType = do
+  let fvs = map (\v -> (v, varTypes M.! v)) freeVars
+  let primFvs = filter (\(_, basicType) -> isPrimitiveType basicType) fvs
+  HORT <$> mkTree primFvs exprType <*> pure exprType
+  where
+    mkTree :: MonadState Int m => [(Var, Type)] -> Type -> m (Tree (Nonterminal, [Type]))
+    mkTree primFreeVars exprType = do
+      let (prims, hots) = partition isPrimitiveType $ flattenType exprType
+      pred <- mkPredicate primFreeVars prims
+      subTrees <- mapM (mkTree []) hots
+      return (Node (pred, prims) subTrees)
 
-buildTreeNode :: MonadState Int m => [(Var, Type)] -> Type ->m (Tree (Nonterminal, [Type]))
-buildTreeNode freeVarMaps exprType = do
-  let (highOrderTypes, basicTypes) = collectTypes ([], []) exprType
-  rootNonterminal <- mkPredicate freeVarMaps basicTypes
-  subTrees <- mapM buildHighOrderTreeNode highOrderTypes
-  return (Node (rootNonterminal , basicTypes) subTrees)
+    mkPredicate :: MonadState Int m => [(Var, Type)] -> [Type] -> m Nonterminal
+    mkPredicate freeVarsWithType types = do
+      idNumber <- get
+      let varList = map (mkArg idNumber) (zip (init types) [1 ..])
+      let outputVar = mkOut idNumber (last types)
+      let freeVarList = map mkFreeVar freeVarsWithType
+      let nonterminal = Nonterminal idNumber (freeVarList ++ varList ++ [outputVar])
+      put (idNumber+1)
+      return nonterminal
 
-buildHighOrderTreeNode :: MonadState Int m => Type ->m (Tree (Nonterminal, [Type]))
-buildHighOrderTreeNode exprType = do
-  let (highOrderTypes, basicTypes) = collectTypes ([], []) exprType
-  rootNonterminal <- mkHOPredicate basicTypes
-  subTrees <- mapM buildHighOrderTreeNode highOrderTypes
-  return (Node (rootNonterminal , basicTypes) subTrees)
+    mkOut :: Int -> Type -> F.Var
+    mkOut pid t = mkFreeVar (Var ("out#" ++ show pid), t)
 
--- | Given a pair of list of types, the first list is the list of high order types,
--- the second list is current primitive type
-collectTypes :: ([Type], [Type]) -> Type -> ([Type], [Type])
-collectTypes (currentHighOrderTypes, basicTypes) currentType = case currentType of
-  TInt -> (currentHighOrderTypes,basicTypes++[TInt])
-  TBool -> (currentHighOrderTypes,basicTypes++[TBool])
-  TArr t1 t2 ->
-    case t1 of
-      TInt -> collectTypes (currentHighOrderTypes, basicTypes++[TInt]) t2
-      TBool -> collectTypes (currentHighOrderTypes, basicTypes++[TBool]) t2
-      TArr _ _ -> collectTypes (currentHighOrderTypes++[t1], basicTypes) t2
-      _ -> error "this type should not be in collectTypes 2"
-  _ -> error "this type should not be in collectTypes 1"
-
-mkHOPredicate :: MonadState Int m => [Type] -> m Nonterminal
-mkHOPredicate = mkPredicate []
-
-mkPredicate :: MonadState Int m => [(Var,Type)] -> [Type] -> m Nonterminal
-mkPredicate freeVarsWithType types = do
-  idNumber <- get
-  let varList = map (mkArg idNumber) (zip (init types) [1 ..])
-  let outputVar = mkOut idNumber (last types)
-  let freeVarList = map mkFreeVar freeVarsWithType
-  let nonterminal = Nonterminal idNumber (freeVarList ++ varList ++ [outputVar])
-  put (idNumber+1)
-  return nonterminal
+flattenType :: Type -> [Type]
+flattenType = \case
+  TInt -> [TInt]
+  TBool -> [TBool]
+  TArr s t -> s : flattenType t
 
 mkFreeVar :: (Var, Type) -> F.Var
 mkFreeVar (Var name, t) = case t of
   TInt -> F.Var name F.Int
   TBool -> F.Var name F.Bool
   _ -> error "it is not an primitive type  free vars (mkFreeVar in HORT)"
-
-mkOut :: Int -> Type -> F.Var
-mkOut pid t = mkFreeVar (Var ("out#" ++ show pid), t)
 
 mkArg :: Int -> (Type,Int) -> F.Var
 mkArg pid (t, idx) = mkFreeVar (Var ("arg" ++ show idx ++ "#" ++ show pid), t)
@@ -125,15 +103,17 @@ constrain constraint witnesses t =
 -- and other types may witness the constraint.
 subtype :: MonadWriter [Rule] m => F.Expr -> [HORT] -> HORT -> HORT -> m ()
 subtype constraint witnesses t' t =
-  tell $ buildSubType constraint (map topPredicate witnesses) (getHORT t') (getHORT t)
+  tell $ subtype' constraint (map topPredicate witnesses) (getHORT t') (getHORT t)
   where
-    buildSubType constraint context (Node (n1, types1) subTrees1) (Node (n2, types2) subTrees2) =
-      let expr = carryBound (length types1) (length types2) n1 n2
-          rule = Rule L n2 (constraint `F.mkAnd` expr) (n1:context)
+    subtype' constraint witnesses
+      (Node (sub, ts1) subTrees1)
+      (Node (super, ts2) subTrees2) =
+      let expr = carryBound (length ts1) (length ts2) sub super
+          rule = Rule L super (constraint `F.mkAnd` expr) (sub:witnesses)
           rules = subTreeRules subTrees2 subTrees1
       in rule : rules
     -- Construct constraints for the subtrees.
-    subTreeRules st1 st2 = concat (zipWith (buildSubType (F.LBool True) []) st1 st2)
+    subTreeRules st1 st2 = concat (zipWith (subtype' (F.LBool True) []) st1 st2)
     -- Copy the bound variables from one type to the other.
     carryBound numBound1 numBound2 (Nonterminal _ vars1) (Nonterminal _ vars2) =
       let toTake = min numBound1 numBound2 in
