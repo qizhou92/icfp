2da17db2ed8af3a9ce077bd23beaedf2d312d4d5
Qi zhou <qizhou1992@gmail.com>
2018-02-28 23:10:11 -0500

    subtype

--- src/Language/RHORT.hs
+++ src/Language/RHORT.hs
@@ -3,11 +3,13 @@ module Language.RHORT where
 
 import           Control.Monad.State
 import           Control.Monad.Writer
+import           Control.Monad
 import           Data.Data (Data)
 import           Data.Tree
 import           Data.Map (Map)
 import qualified Data.Map as M
 import qualified Data.List as L
+import qualified Data.Set as S
 
 import           Language.Types
 import           Grammar
@@ -20,21 +22,21 @@ data RHORT = RHORT
 -- getBasicTypes returns all basic types for the vector of expressions
    , getBasicTypes :: [Type]
    , getVarLength :: Int
-  } deriving (Show, Read, Eq, Data)
+  } deriving (Show, Read, Eq, Data,Ord)
 
 -- RHORTNode is the node of the RHORT DAG, get predicate returns the nonterminal if it
 -- is a leaf node, getEdges returns the outgoing edge of this node
 data RHORTNode = RHORTNode 
-   {  getPredicate    :: (Maybe Nonterminal) 
+   {  getPredicate    :: (Maybe ([Int],Nonterminal)) 
     , getEdges        :: [RHORTEdge]
-   } deriving (Show,Read,Eq,Data)
+   } deriving (Show,Read,Eq,Data,Ord)
 
 -- RHORTEdge is the directed edge of RHORT DAG, get indexs return the index list of this unwinding
 -- getNodes returns the RHORT nodes this edge points to.
 data RHORTEdge = RHORTEdge
     {  getIndexs :: [Int]
      , getNodes  :: [RHORTNode]
-    } deriving (Show,Read,Eq,Data)
+    } deriving (Show,Read,Eq,Data,Ord)
 
 -- |Given a relational higher order refinement type, the unique id of the expression and the corresponding index, 
 -- fetch the formula (variable) which represents the value of the expression.
@@ -125,9 +127,13 @@ getDAGNode  varPairs exprIds exprTypes = do
   visitedMap <- get
   case (M.lookup indexList visitedMap) of
     Just dagNode -> return dagNode
-    Nothing -> freshDAGNode varPairs exprIds exprTypes
-  where
-    getFlatTypeId flatType = case flatType of
+    Nothing -> do 
+                newDAGNode <- freshDAGNode varPairs exprIds exprTypes
+                put (M.insert indexList newDAGNode visitedMap)
+                return newDAGNode
+    
+getFlatTypeId :: FlatType -> Int
+getFlatTypeId flatType = case flatType of
       FlatType i _ -> i
       FlatTypeArr i _ _ -> i
 
@@ -135,12 +141,13 @@ freshDAGNode ::  MonadState Int m => [(Var,FlatType)] -> [Int] -> [FlatType] -> 
 freshDAGNode varPairs exprIds exprTypes = do
   let varTypes = map (\(_,t) -> t) varPairs
   let flatTypeList = varTypes ++ exprTypes 
-  let possibleIndexs = filter (\(t,_) ->not (isPrimFlatType t)) (zip flatTypeList [0 .. ]) 
+  let possibleIndexs = filter (\(t,_) ->not (isPrimFlatType t)) (zip flatTypeList ([0 .. ]::[Int])) 
   if (null possibleIndexs)
     then do 
            let varName = map(\(v,_) -> v) varPairs
            predicate <- (lift (mkPredicate flatTypeList varName exprIds))
-           return (RHORTNode (Just predicate) [])
+           let flatIdList = map getFlatTypeId flatTypeList
+           return (RHORTNode (Just (flatIdList,predicate)) [])
     else do 
           let allPossibleIndex = getAllPossibleIndex (map (\(_,i)->i) possibleIndexs)
           allEdges <- mapM (constructEdge varPairs exprIds exprTypes) allPossibleIndex
@@ -220,6 +227,82 @@ split index rhort = case (safeGet "split is over index" index (getBasicTypes rho
                      rightRHORT = (RHORT rightNode newRightTypes varLength)
                    in (leftRHORT,rightRHORT)
   _ ->  error "not a supported type (split in RHORT)"
+
+
+subtype :: MonadWriter [Rule] m => F.Expr -> RHORT -> RHORT -> m ()
+subtype = undefined
+
+subtype' :: MonadWriter [Rule] m => [(Int,Int)] -> F.Expr -> [F.Var] -> RHORTNode -> RHORTNode -> m ()
+subtype' indexPairs constraint fVars node1 node2 = case (getPredicate node1) of
+  Just (_,nonterminal) -> tell $ [buildConstrains constraint fVars nonterminal (getPredicate node2)]
+  Nothing -> do
+               let edges1 = getEdges node1
+               let edges2 = getEdges node2 
+               let allPossibleEdgePairs = concat (map (\x -> (map (\y -> (x,y)) edges1)) edges2)
+               let sameEdgePairs = filter (\(edge1,edge2) -> sameIndex indexPairs (getIndexs edge1) (getIndexs edge2)) allPossibleEdgePairs
+               let (leftNodes1,leftNodes2) = getLeftNodes sameEdgePairs
+               let (rightNodes1,rightNodes2) = getRightNodes sameEdgePairs
+               _ <- zipWithM (subtype' indexPairs constraint fVars) leftNodes2 leftNodes1
+               _ <- zipWithM (subtype' indexPairs constraint fVars) rightNodes1 rightNodes2
+               return ()
+
+getLeftNodes :: [(RHORTEdge,RHORTEdge)] -> ([RHORTNode],[RHORTNode])
+getLeftNodes = foldr (\(edge1,edge2) (list1,list2) -> ( ((getLeftNode edge1):list1) , ((getLeftNode edge2):list2) ) ) ([],[])
+  where getLeftNode edge = safeGet "there is no right node in getLeftNodes" 0 (getNodes edge)
+
+getRightNodes :: [(RHORTEdge,RHORTEdge)] -> ([RHORTNode],[RHORTNode])
+getRightNodes = foldr (\(edge1,edge2) (list1,list2) ->( ((getRightNode edge1):list1) , ((getRightNode edge2):list2) ) ) ([],[])
+   where getRightNode edge = safeGet "there is no right node in getLeftNodes" 1 (getNodes edge)
+
+buildConstrains :: F.Expr -> [F.Var] -> Nonterminal -> Maybe ([Int],Nonterminal) -> Rule
+buildConstrains constraint fVars nonterminal1 predicate = case predicate of
+  Just (_,nonterminal2) -> do
+                             let (Nonterminal _ vars1) = nonterminal1
+                             let (Nonterminal _ vars2) = nonterminal2
+                             let equalExpr = buildEqExpr fVars vars1 vars2
+                             Rule L nonterminal1 (equalExpr `F.mkAnd` constraint) [nonterminal2]
+  Nothing -> error "the second hort does not match the first hort"
+
+buildEqExpr :: [F.Var] -> [F.Var] -> [F.Var] -> F.Expr
+buildEqExpr fVars vars1 vars2 = 
+  let list1 = (filter (\x -> L.notElem x  fVars) vars1)
+      list2 = (filter (\x -> L.notElem x  fVars) vars2)
+   in F.manyAnd (zipWith (\x y -> [F.expr|@x = @y|]) list1 list2)
+
+
+
+
+-- given two same HORT, get the list of corresponding pairs of nonterminals
+samePositionPair :: [(Int,Int)] -> RHORT -> RHORT -> [(Nonterminal,Nonterminal)]
+samePositionPair indexPairs rhort1 rhort2 = 
+  let allNonterminalsIn1 = getAllNonterminals rhort1
+      allNonterminalsIn2 = getAllNonterminals rhort2
+      allPossiblePairs = concat (map (\x -> (map (\y -> (x,y)) allNonterminalsIn2)) allNonterminalsIn1)
+      samePairs = filter (\((index1,_),(index2,_)) -> sameIndex indexPairs index1 index2) allPossiblePairs
+    in (map (\((_,n1),(_,n2)) -> (n1,n2)) samePairs)
+
+sameIndex :: [(Int,Int)] -> [Int] -> [Int] -> Bool
+sameIndex pairsIndex index1 index2 = 
+  let samePairs = takeWhile (\(t1,t2)-> (safeGet "sameIndex is wrong" t1 index1) == (safeGet "sameIndex is wrong" t2 index2)) pairsIndex
+    in (length samePairs) == (length pairsIndex)
+
+getAllNonterminals :: RHORT -> [([Int],Nonterminal)]
+getAllNonterminals rhort = 
+  let allNodes = S.toList (execState (collectAllNodes (getRHORT rhort)) S.empty)
+     in concat (map getNonterminal allNodes)
+  where 
+    getNonterminal node = case (getPredicate node) of
+      Just a -> [a]
+      Nothing -> []
+
+collectAllNodes :: RHORTNode -> State (S.Set RHORTNode) ()
+collectAllNodes node = do
+  visitedNodes <- get
+  if S.member node visitedNodes then return ()
+    else do
+           put (S.insert node visitedNodes) 
+           _ <- mapM (collectAllNodes) (concat (map getNodes (getEdges node)))
+           return ()
 
 -- | print an given the message is current list is less then the index
 safeGet :: String -> Int -> [a] -> a
